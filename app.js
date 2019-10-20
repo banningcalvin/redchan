d4505ecf9b6c77f59e319cc63bbc41907dd6dfc6
cb <cbanning@udel.edu>
2019-10-20 13:15:16 -0400

    ejs updates

--- app.js
+++ app.js
@@ -1,14 +1,48 @@
 const express = require('express');
 const http = require('http')
 const app = express();
+const firebase = require('firebase')
 const port = 3000;
 
 var server = http.createServer(app);
-app.use(express.static('public'))
+
+app.set('view engine', 'ejs');
+app.use(express.static(__dirname + '/public'));
+
+var firebaseConfig = {
+    apiKey: "AIzaSyC5A4oZaEJBNyzJdSvX_ST2_pI_-4bXOP4",
+    authDomain: "redchan-8d5ab.firebaseapp.com",
+    databaseURL: "https://redchan-8d5ab.firebaseio.com",
+    projectId: "redchan-8d5ab",
+    storageBucket: "redchan-8d5ab.appspot.com",
+    messagingSenderId: "575273642415",
+    appId: "1:575273642415:web:95ab486312aa6eb4b5e75a"
+};
+// Initialize Firebase
+var fb = firebase.initializeApp(firebaseConfig);
+var database = firebase.database();
+
+app.get('/', function(req, res) {
+    res.render('pages/index');
+});
+
+app.get('/login', function(req, res) {
+    res.render('pages/login');
+});
+
 
 app.get("/board/:board", function(req, res){
-    res.send(`get /board/${req.params.board}`);
- });
+    firebase.database().ref('/board/' + req.params.board).once('value').then(function(snapshot) {
+        console.log(snapshot.val())
+        //res.send(`get /board/${req.params.board} ${snapshot.val()}`);
+        let data = snapshot.val()
+        res.render('pages/board', {
+            title: "/" + req.params.board + "/ - " + data.title,
+            description: data.description,
+            posts: data.posts
+        })
+    });
+});
 
 
 app.listen(3000, "localhost")
--- public/index.js
+++ public/index.js
@@ -1,21 +1,6 @@
 $(function () {
     // Your web app's Firebase configuration
-    var firebase = require('firebase');
-
-    var firebaseConfig = {
-        apiKey: "AIzaSyC5A4oZaEJBNyzJdSvX_ST2_pI_-4bXOP4",
-        authDomain: "redchan-8d5ab.firebaseapp.com",
-        databaseURL: "https://redchan-8d5ab.firebaseio.com",
-        projectId: "redchan-8d5ab",
-        storageBucket: "redchan-8d5ab.appspot.com",
-        messagingSenderId: "575273642415",
-        appId: "1:575273642415:web:95ab486312aa6eb4b5e75a"
-    };
-    // Initialize Firebase
-    var app = firebase.initializeApp(firebaseConfig);
-
-
-    $("#nav").load("nav.html");
+    //var firebase = require('firebase');
 
     
 });
